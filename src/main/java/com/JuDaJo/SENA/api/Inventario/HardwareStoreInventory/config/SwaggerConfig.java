90d3f7bb22b63b6f74edef342a1dd268800b6a5b
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-19 18:05:41 -0500

    Se modifica la clase SwaggerConfig con respecto a la licencia de uso

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/config/SwaggerConfig.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/config/SwaggerConfig.java
@@ -15,16 +15,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("API REST de Inventario-hardwareStoreInventory")
                         .version("1.0")
-                        .description("Documentación de la API REST para gestionar el inventario de la ferreteria FFIG.")
+                        .description("Documentación de la API REST para gestionar el inventario de la ferreteria FFIG \n\n"
+                                + "Esta API utiliza las siguientes herramientas:\n"
+                                + "- Java JDK17: [Descargar aquí](https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html)\n"
+                                + "- Spring Boot 3.4.3: [Visita Spring Initializr](https://start.spring.io/)\n"
+                                + "- MySQL 9.1.0: [Descargar desde MySQL Archives](https://downloads.mysql.com/archives/community/)")
                         .license(new License()
-                                .name("Java JDK17")
-                                .url("https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html"))
-                        .license(new License()
-                                .name("SpringBoot 3.4.3")
-                                .url("https://start.spring.io/"))
-                        .license(new License()
-                                .name("MySql 9.1.0")
-                                .url("https://downloads.mysql.com/archives/community/"))
+                                .name("JuDaJo - Sena Centro de Comercio y Turismo Armenía, Regional Quindío 2024 - 2025"))
                         .contact(new Contact()
                                 .name("David Ricardo Graffe Rodríguez Ficha: 2879724, Joaquín Humberto Jiménez Rosas y Juan David Gallego López Ficha: 2879723")
                                 .email("Joaquin_Jimenez@soy.sena.edu.co"))
