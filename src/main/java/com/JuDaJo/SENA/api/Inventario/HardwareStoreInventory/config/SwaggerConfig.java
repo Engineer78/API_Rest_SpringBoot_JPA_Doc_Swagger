748e775e28c2e2558699e304eb8d0a386462187c
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-19 17:12:13 -0500

    Se crea la clase SwaggerConfig dentro del package config del proyecto y  se agregan las importaciones requeridas

---
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class SwaggerConfig {
+}
