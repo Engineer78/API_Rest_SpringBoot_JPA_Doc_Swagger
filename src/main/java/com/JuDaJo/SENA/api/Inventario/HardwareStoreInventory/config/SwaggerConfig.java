60cb997cf7a2bf631fcf3352d7133bb1017dfb28
Joaquin Jimenez <jjimenezr78@gmail.com>
2025-03-19 17:38:43 -0500

    Se agrega la configuración básica a la clase SwaggerConfig para la documentación de la API Rest

--- src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/config/SwaggerConfig.java
+++ src/main/java/com/JuDaJo/SENA/api/Inventario/HardwareStoreInventory/config/SwaggerConfig.java
@@ -1,9 +1,33 @@
 package com.JuDaJo.SENA.api.Inventario.HardwareStoreInventory.config;
 
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API REST de Inventario-hardwareStoreInventory")
+                        .version("1.0")
+                        .description("Documentación de la API REST para gestionar el inventario de la ferreteria FFIG.")
+                        .license(new License()
+                                .name("Java JDK17")
+                                .url("https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html"))
+                        .license(new License()
+                                .name("SpringBoot 3.4.3")
+                                .url("https://start.spring.io/"))
+                        .license(new License()
+                                .name("MySql 9.1.0")
+                                .url("https://downloads.mysql.com/archives/community/"))
+                        .contact(new Contact()
+                                .name("David Ricardo Graffe Rodríguez Ficha: 2879724, Joaquín Humberto Jiménez Rosas y Juan David Gallego López Ficha: 2879723")
+                                .email("Joaquin_Jimenez@soy.sena.edu.co"))
+                );
+    }
 }
